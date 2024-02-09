f2554584d076a4bbc25d9bde1f8db3d4398bf00f
Jesper Wallén <jesperwallen2001@gmail.com>
2024-02-09 09:03:24 +0100

    added more tests

--- TestxUnit/xUnitTests.cs
+++ TestxUnit/xUnitTests.cs
@@ -24,5 +24,25 @@ namespace Caesar
 
             Assert.NotEqual(expected, result);
         }
+        [Theory]
+        [InlineData("ABC", 3, "ÅÄÖ")]
+        [InlineData("HEJ", 3, "EBG")]
+        [InlineData("Vad gör du?", 3, "Såa dzo ar?")]
+        public void DecryptCaesarTest_ShouldReturnCorrect(string text, int shift, string expected)
+        {
+            string result = Caesar.CaesarMachine.DecryptCaesar(text, shift);
+
+            Assert.Equal(expected, result);
+        }
+        [Theory]
+        [InlineData("ÅÄÖ", 3, "ABC")]
+        [InlineData("TYIO1", 3, "HEJDÅ")]
+        [InlineData("SDas eiafj?", 3, "Vad gör du?")]
+        public void DecryptCaesarTest_ShouldReturnFalse(string text, int shift, string expected)
+        {
+            string result = Caesar.CaesarMachine.DecryptCaesar(text, shift);
+
+            Assert.NotEqual(expected, result);
+        }
     }
 }
